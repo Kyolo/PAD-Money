fd819fe09804be67f7e3721d7478740cfb6f87e5
peonzz <adrien_eb@orange.fr>
2017-05-23 12:30:39 +0200

    Codage budgetPrevi

--- PAD-Money/PAD-Money/Form1.cs
+++ PAD-Money/PAD-Money/Form1.cs
@@ -15,6 +15,7 @@ namespace PAD_Money
     {
         public string CH_CON = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=";
         public OleDbConnection connec;
+        private DataSet ds;
 
         public frmMain()
         {
@@ -38,12 +39,40 @@ namespace PAD_Money
             if(ofd.ShowDialog() == DialogResult.OK)
             {
                 connec = new OleDbConnection(CH_CON + ofd.FileName);
+                try
+                {
+                    connec.Open();
+                    ds = new DataSet();
+                    DataTable schema = connec.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                    OleDbDataAdapter da = new OleDbDataAdapter();
+                    //import de la base de donnée en local
+                    for (int i = 0; i < schema.Rows.Count; i++)
+                    {
+                        //on récupère le nom des tables
+                        String requete = "SELECT * FROM [" + schema.Rows[i].ItemArray[2]+"]";
+                        OleDbCommand com = new OleDbCommand(requete, connec);
+                        da.SelectCommand = com;
+                        da.Fill(ds, schema.Rows[i].ItemArray[2].ToString());
+
+                    }
+                }
+                catch(Exception erreur)
+                {
+                    MessageBox.Show(erreur.Message);
+                }
+                finally
+                {
+                    if(connec.State == ConnectionState.Open)
+                    {
+                        connec.Close();
+                    }
+                }
             }
         }
 
         private void btnBudgetPrevi_Click(object sender, EventArgs e)
         {
-            FrmBudgetPrevi frm = new FrmBudgetPrevi(connec);
+            FrmBudgetPrevi frm = new FrmBudgetPrevi(connec, ds);
             frm.ShowDialog();
         }
     }
--- PAD-Money/PAD-Money/frmBudgetMois.Designer.cs
+++ PAD-Money/PAD-Money/frmBudgetMois.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 427);
+            this.ClientSize = new System.Drawing.Size(784, 441);
             this.Name = "FrmBudgetMois";
             this.Text = "frmBudgetMois";
             this.ResumeLayout(false);
