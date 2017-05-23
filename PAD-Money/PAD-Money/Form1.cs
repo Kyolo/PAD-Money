793150e7ad2f1e5b6102e00007f4cf02b1203d9d
Kyolo <kyolo@Kyolo-PCARL>
2017-05-23 16:35:44 +0200

    Améliorations sur le menu

--- PAD-Money/PAD-Money/Form1.cs
+++ PAD-Money/PAD-Money/Form1.cs
@@ -13,9 +13,13 @@ namespace PAD_Money
 {
     public partial class frmMain : Form
     {
-        public string CH_CON = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=";
-        public OleDbConnection connec;
+        public const string CH_CON = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=";
+        private OleDbConnection connec;
         private DataSet ds;
+
+        private FrmBudgetMois budgetMois = null;
+
+        private FrmBudgetPrevi budgetprevi = null;
 
         public frmMain()
         {
@@ -29,25 +33,32 @@ namespace PAD_Money
 
         private void btnBudgetMois_Click(object sender, EventArgs e)
         {
-            FrmBudgetMois frm = new FrmBudgetMois(connec, ds);
-            frm.ShowDialog();
+            if(budgetMois == null)
+                budgetMois = new FrmBudgetMois(connec,ds);
+            budgetMois.ShowDialog();
         }
 
-        private void btnOuvrirBase_Click(object sender, EventArgs e)
+        private void btnBudgetPrevi_Click(object sender, EventArgs e)
         {
+            //On stock les formulaire en locale pour ne pas avoir à le remplir à chaques fois qu'on les affiches
+            if(budgetprevi == null)
+                budgetprevi = new FrmBudgetPrevi(connec,ds);
+            budgetprevi.ShowDialog();
+        }
+
+
+        private void btnOuvrirBase_Click(object sender, EventArgs e) {
+
             OpenFileDialog ofd = new OpenFileDialog();
-            if(ofd.ShowDialog() == DialogResult.OK)
-            {
+            if(ofd.ShowDialog() == DialogResult.OK) {
                 connec = new OleDbConnection(CH_CON + ofd.FileName);
-                try
-                {
+                try {
                     connec.Open();
                     ds = new DataSet();
                     DataTable schema = connec.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
                     OleDbDataAdapter da = new OleDbDataAdapter();
                     //import de la base de donnée en local
-                    for (int i = 0; i < schema.Rows.Count; i++)
-                    {
+                    for (int i = 0; i < schema.Rows.Count; i++) {
                         //on récupère le nom des tables
                         String requete = "SELECT * FROM [" + schema.Rows[i].ItemArray[2]+"]";
                         OleDbCommand com = new OleDbCommand(requete, connec);
@@ -56,24 +67,16 @@ namespace PAD_Money
 
                     }
                 }
-                catch(Exception erreur)
-                {
-                    MessageBox.Show(erreur.Message);
+                catch(Exception erreur) {
+                    MessageBox.Show("Erreur en remplissant la table :\n"+erreur.Message);
                 }
-                finally
-                {
-                    if(connec.State == ConnectionState.Open)
-                    {
+                finally {
+                    if(connec.State == ConnectionState.Open) {
                         connec.Close();
                     }
                 }
             }
         }
 
-        private void btnBudgetPrevi_Click(object sender, EventArgs e)
-        {
-            FrmBudgetPrevi frm = new FrmBudgetPrevi(connec, ds);
-            frm.ShowDialog();
-        }
     }
 }
